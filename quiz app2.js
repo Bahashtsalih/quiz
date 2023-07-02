081026775a45b5f4bbcc0f773d8957340cf7bb1c
Bahashtsalih <86877971+Bahashtsalih@users.noreply.github.com>
2023-07-02 22:17:16 +0300

    Add files via upload

---
+++ quiz app2.js
@@ -0,0 +1,30 @@
+
+ 
+   //Questions array
+   let questions = [
+    {
+         number:1,
+         question: "1 +1 ؟",
+         answer : "2",
+         options : [
+                          "3",
+                          "4",
+                          "6",
+                          "2"
+                        ]
+    
+    },
+    {
+         number:2,
+         question: "دایکی پێغەمبەر (ص) ناوی چی بوو ؟",
+         answer : "ئامینە",
+         options : [ 
+                          "فاتیمە",
+                          "ئامینە",
+                          "عائیشە",
+                          "زەینەب "
+                          ]
+    
+                        }
+
+     ];    
